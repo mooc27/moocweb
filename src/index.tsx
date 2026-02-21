7cddc75b68c5cc5d91fb5f424be3fcdc3a44184c
nanaNahida <beier.zheng@outlook.com.au>
2026-02-21 20:57:02 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import store from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-)
+);
 root.render(
   <React.StrictMode>
      <Provider store={store}>
@@ -22,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
